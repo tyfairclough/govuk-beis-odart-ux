bde402931a8f07e8ec2d5da25c5030b5998681e4
Ty Fairclough <ty.fairclough@gmail.com>
2019-05-15 14:46:44 +0100

    report UI v0.1

--- app/views/apps/_wip_odart_130519/assets/javascripts/sub-application.js
+++ app/views/apps/_wip_odart_130519/assets/javascripts/sub-application.js
@@ -79,6 +79,168 @@ switch (pageName) {
 
 function biesReporting(){
   $("body").addClass("wide");
+
+
+// accordion
+
+$(".related .relatedBody").addClass("hide");
+
+
+$(".related .relatedHeader").click(function(e){
+  $(this).next().toggleClass("hide");
+  $(this).children(".fa").toggleClass("fa-plus");
+  $(this).children(".fa").toggleClass("fa-minus");
+  // $(this).parent($("i").toggleClass("fa-minus"));
+  // $(this).parent().css("background","red");
+  // $(this).parent().css("background","red").closest("i").toggleClass("fa-plus");
+  // $(".relatedHeader i", this).toggleClass("fa-minus");
+});
+
+// filter drop downs
+
+$('#select-region').selectize({
+});
+$('#select-country').selectize({
+  maxItems: 3
+});
+$('#select-approval').selectize({
+  maxItems: 3
+});
+$('#select-activity').selectize({
+  maxItems: 3
+});
+$('#select-dp').selectize({
+  maxItems: 10
+});
+
+$('#select-keywords').selectize({
+    delimiter: ',',
+    persist: false,
+    create: function(input) {
+        return {
+            value: input,
+            text: input
+        }
+    }
+})
+
+
+var $width = $("#fundDistribution").parent().width()
+
+  // set the dimensions and margins of the graph
+  var margin = {top: 80, right: 30, bottom: 50, left:110},
+      // width = 460 - margin.left - margin.right,
+      width = $("#fundDistribution").parent().width() - margin.left - margin.right,
+      height = 400 - margin.top - margin.bottom;
+
+  // append the svg object to the body of the page
+  var svg = d3.select("#fundDistribution")
+    .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+  //read data
+  d3.csv("/public/assets/ebolaChart.csv", function(data) {
+
+    // Get the different categories and count them
+    var categories = ["Congo", "Cote d'Ivoire", "DR of Congo", "Gabon", "Guinea", "Leone", "Liberia", "Mali", "Nigeria","Senegal","Sierra Leone","South Africa","South Sudan","Uganda" ]
+    var n = categories.length
+
+    // Compute the mean of each group
+    allMeans = []
+    for (i in categories){
+      currentGroup = categories[i]
+      mean = d3.mean(data, function(d) { return +d[currentGroup] })
+      allMeans.push(mean)
+    }
+
+    // Create a color scale using these means.
+    var myColor = d3.scaleSequential()
+      .domain([0,100])
+      .interpolator(d3.interpolateViridis);
+
+    // Add X axis
+    var x = d3.scaleLinear()
+      .domain([-10, 120])
+      .range([ 0, width ]);
+    svg.append("g")
+      .attr("class", "xAxis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x).tickValues([0,25, 50, 75, 100]).tickSize(-height) )
+      .select(".domain").remove()
+
+    // Add X axis label:
+    svg.append("text")
+        .attr("text-anchor", "end")
+        .attr("x", width)
+        .attr("y", height + 40)
+        .text("Spend over time (days)");
+
+    // Create a Y scale for densities
+    var y = d3.scaleLinear()
+      .domain([0, 0.25])
+      .range([ height, 0]);
+
+    // Create the Y axis for names
+    var yName = d3.scaleBand()
+      .domain(categories)
+      .range([0, height])
+      .paddingInner(1)
+    svg.append("g")
+      .call(d3.axisLeft(yName).tickSize(0))
+      .select(".domain").remove()
+
+    // Compute kernel density estimation for each column:
+    var kde = kernelDensityEstimator(kernelEpanechnikov(7), x.ticks(40)) // increase this 40 for more accurate density.
+    var allDensity = []
+    for (i = 0; i < n; i++) {
+        key = categories[i]
+        density = kde( data.map(function(d){  return d[key]; }) )
+        allDensity.push({key: key, density: density})
+    }
+
+    // Add areas
+    svg.selectAll("areas")
+      .data(allDensity)
+      .enter()
+      .append("path")
+        .attr("transform", function(d){return("translate(0," + (yName(d.key)-height) +")" )})
+        .attr("fill", function(d){
+          grp = d.key ;
+          index = categories.indexOf(grp)
+          value = allMeans[index]
+          return myColor( value  )
+        })
+        .datum(function(d){return(d.density)})
+        .attr("opacity", 0.7)
+        .attr("stroke", "#000")
+        .attr("stroke-width", 0.1)
+        .attr("d",  d3.line()
+            .curve(d3.curveBasis)
+            .x(function(d) { return x(d[0]); })
+            .y(function(d) { return y(d[1]); })
+        )
+
+  })
+
+  // This is what I need to compute kernel density estimation
+  function kernelDensityEstimator(kernel, X) {
+    return function(V) {
+      return X.map(function(x) {
+        return [x, d3.mean(V, function(v) { return kernel(x - v); })];
+      });
+    };
+  }
+  function kernelEpanechnikov(k) {
+    return function(v) {
+      return Math.abs(v /= k) <= 1 ? 0.75 * (1 - v * v) / k : 0;
+    };
+  }
+
+
 }
 
 function odartLanding(){
